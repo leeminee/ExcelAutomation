0d7766382da49c91b984899ea6cf21fa8c685614
Minee <mingramer@gmail.com>
2021-02-23 01:07:15 +0900

    엑셀 파일읽기 간단히1

--- 엑셀파일읽기.py
+++ 엑셀파일읽기.py
@@ -10,3 +10,26 @@ print(load_ws['A1'].value)
 
 # 셀 좌표로 값 출력
 print(load_ws.cell(1,2).value)
+
+print('\n------지정한 셀 출력------')
+get_cells = load_ws['A1':'D2']
+for row in get_cells:
+    for cell in row:
+        print(cell.value)
+
+print('\n------모든 행 단위로 출력------')
+for row in load_ws.rows:
+    print(row)
+
+print('\n------모든 열 단위로 출력------')
+for column in load_ws.columns:
+    print(column)
+
+print('\n------모든 행과 열 출력------')
+all_values = []
+for row in load_ws.rows:
+    row_value = []
+    for cell in row:
+        row_value.append(cell.value)
+    all_values.append(row_value)
+print(all_values)

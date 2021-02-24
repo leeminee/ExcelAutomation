94213723ae03fe3714e300dc199f73ed0af79229
Minee <mingramer@gmail.com>
2021-02-24 22:01:04 +0900

    엑셀 파일쓰기 간단히1

---
+++ 엑셀파일쓰기.py
@@ -0,0 +1,17 @@
+from openpyxl import Workbook
+
+write_wb = Workbook()
+
+# 이름이 있는 시트를 생성
+# write_ws = write_wb.create_sheet('생성시트')
+
+# Sheet1에다 입력
+write_ws = write_wb.active
+write_ws['A1'] = '숫자'
+
+# 행 단위로 추가
+write_ws.append([1,2,3])
+
+# 셀 단위로 추가
+write_ws.cell(5,5,'5행5열')
+write_wb.save('C:/test/숫자.xlsx')

b7a945dc93edd336adfe00a68edf7dfc1a646ee4
Minee <mingramer@gmail.com>
2021-02-28 17:57:04 +0900

    엑셀 파일쓰기_실전 ( 다음엔 디비에서 연동해보기 )

--- 엑셀파일쓰기_실전.py
+++ 엑셀파일쓰기_실전.py
@@ -2,3 +2,11 @@ from openpyxl import Workbook
 
 write_wb = Workbook()
 
+write_ws = write_wb.create_sheet('Alarm 현황')
+write_ws['C2'] = 'Host'
+write_ws.cell(2,4,'Message1')
+write_ws.cell(2,5,'Message2')
+write_ws.cell(2,6,'합계')
+
+write_wb.save('C:/test/Alarm현황.xlsx')
+

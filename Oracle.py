657dcbe76f092ba08f798dfdbdab26e5a56a521b
Minee <mingramer@gmail.com>
2021-03-01 17:04:27 +0900

    Oracle 연동 중 에러남..

---
+++ Oracle.py
@@ -0,0 +1,22 @@
+import cx_Oracle
+
+class Test:
+    def __init__(self, uuid, host, msg1, msg2):
+        self.uuid = uuid
+        self.host = host
+        self.msg1 = msg1
+        self.msg2 = msg2
+
+def select_all():
+    conn = cx_Oracle.connect('mllee', 'mllee', 'localhost/orcl')
+    try:
+        with conn.cursor() as curs:
+            sql = "select * from alarm_test"
+            curs.execute(sql)
+            rs = curs.fetchall()
+            for row in rs:
+                print(row)
+    finally:
+        conn.close()
+
+select_all()

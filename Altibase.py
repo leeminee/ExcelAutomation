8336d9d30eeb0bf7c5d9a95ba88795d4da6ac46d
Minee <mllee@ipageon.com>
2021-03-08 16:59:59 +0900

    Python에 Altibase 연동해서 데이터 가져오기 하는 중

---
+++ Altibase.py
@@ -0,0 +1,48 @@
+import jaydebeapi, jpype
+from openpyxl import Workbook
+
+classname = 'Altibase.jdbc.driver.AltibaseDriver'
+classfile = 'C:/Users/user/IdeaProjects/lib/Altibase6_5.jar'
+dbuser = 'sys'
+passwd = 'manager'
+
+conn, cursor = None, None
+
+
+def select_all():
+    try:
+        conn = jaydebeapi.connect(classname, ['jdbc:Altibase://10.200.2.130:20300/mydb', '%s' % dbuser, '%s' % passwd],classfile)
+        cursor = conn.cursor()
+        print("conn : " + conn)
+        print(">> Altibase connection")
+
+        sql = "SELECT * FROM STAT_MCPPTT_SUBS ORDER BY STIME DESC LIMIT 1"
+        cursor.execute(sql)
+
+        data = cursor.fetchall()
+
+        for x in data:
+            print(x[0])
+            print(x[1])
+            print(x[2])
+
+        cursor.close()
+        conn.close()
+    except Exception as msg:
+        print('Error Message : %s' %msg)
+
+    # cursor = conn.cursor()
+    #
+    # cursor.execute("select * from alarm_test")
+    #
+    # row = cursor.fetchone()
+    #
+    # while row:
+    #     print("uuid : "+row[0] + ", host : "+row[1]+", msg1 : "+row[2]+", msg2 : "+row[3])
+    #     row = cursor.fetchone()
+    # print()
+    # conn.close()
+
+
+select_all()
+# dg4odbc

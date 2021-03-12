63a360105b9607560700b055ec483ae49e14c4f7
Dong-uk, Kim <eastiris@gmail.com>
2021-03-13 02:10:33 +0900

    Argument 수정. 	modified:   Altibase.py 	new file:   altibase-jdbc-driver-5.1.5.46.jar

--- Altibase.py
+++ Altibase.py
@@ -2,7 +2,9 @@ import jaydebeapi, jpype, pyodbc
 from openpyxl import Workbook
 
 classname = 'Altibase.jdbc.driver.AltibaseDriver'
-classfile = 'C:/Users/user/IdeaProjects/lib/Altibase6_5.jar'
+# classfile = 'C:/Users/user/IdeaProjects/lib/Altibase6_5.jar'
+classfile = 'altibase-jdbc-driver-5.1.5.46.jar'
+
 dbuser = 'ipageon'
 passwd = 'ipageon'
 
@@ -11,12 +13,15 @@ conn, cursor = None, None
 
 def select_all():
     try:
-        cnxn = pyodbc.connect('DRIVER={Altibase.jdbc.driver.AltibaseDriver}; SERVER=localhost; DATABASE=mbdb; UID=ipageon; PWD=ipageon')
-        cursor = cnxn.cursor()
-        cnxn = pyodbc.connect('DSN=mydb; PWD=ipageon')
-        # conn = jaydebeapi.connect(classname, ['jdbc:Altibase://10.200.2.130:20300/mydb', '%s' % dbuser, '%s' % passwd],classfile)
-        # cursor = conn.cursor()
-        # print("conn : " + conn)
+        # cnxn = pyodbc.connect('DRIVER={Altibase.jdbc.driver.AltibaseDriver}; SERVER=localhost; DATABASE=mbdb; UID=ipageon; PWD=ipageon')
+        # cursor = cnxn.cursor()
+        # cnxn = pyodbc.connect('DSN=mydb; PWD=ipageon')
+
+        # conn = jaydebeapi.connect(classname, ['jdbc:Altibase://10.200.2.130:20300/mydb', '%s' % dbuser, '%s' % passwd], classfile)
+        conn = jaydebeapi.connect( classname, "jdbc:Altibase://10.200.2.130:20300/mydb", [dbuser, passwd], classfile)
+
+        cursor = conn.cursor()
+        print("conn : " + conn)
         print(">> Altibase connection")
 
         sql = "SELECT * FROM STAT_MCPPTT_SUBS ORDER BY STIME DESC LIMIT 1"

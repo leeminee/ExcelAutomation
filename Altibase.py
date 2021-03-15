440c3adefe5e0c8f2dfb5f98805eb4c5338896b3
Minee <mllee@ipageon.com>
2021-03-15 10:06:59 +0900

    Altibase 연동성공 Altibase.jdbc.driver.AltibaseDriver -> Altibase6_5.jdbc.driver.AltibaseDriver 로 수정

--- Altibase.py
+++ Altibase.py
@@ -1,9 +1,8 @@
 import jaydebeapi, jpype, pyodbc
 from openpyxl import Workbook
 
-classname = 'Altibase.jdbc.driver.AltibaseDriver'
-# classfile = 'C:/Users/user/IdeaProjects/lib/Altibase6_5.jar'
-classfile = 'altibase-jdbc-driver-5.1.5.46.jar'
+classname = 'Altibase6_5.jdbc.driver.AltibaseDriver'
+classfile = 'C:/Users/user/IdeaProjects/lib/Altibase6_5.jar'
 
 dbuser = 'ipageon'
 passwd = 'ipageon'
@@ -13,43 +12,23 @@ conn, cursor = None, None
 
 def select_all():
     try:
-        # cnxn = pyodbc.connect('DRIVER={Altibase.jdbc.driver.AltibaseDriver}; SERVER=localhost; DATABASE=mbdb; UID=ipageon; PWD=ipageon')
-        # cursor = cnxn.cursor()
-        # cnxn = pyodbc.connect('DSN=mydb; PWD=ipageon')
-
-        # conn = jaydebeapi.connect(classname, ['jdbc:Altibase://10.200.2.130:20300/mydb', '%s' % dbuser, '%s' % passwd], classfile)
-        conn = jaydebeapi.connect( classname, "jdbc:Altibase://10.200.2.130:20300/mydb", [dbuser, passwd], classfile)
+        conn = jaydebeapi.connect(classname, "jdbc:Altibase://10.200.2.130:20300/mydb", [dbuser, passwd], classfile)
 
         cursor = conn.cursor()
-        print("conn : " + conn)
         print(">> Altibase connection")
 
-        sql = "SELECT * FROM STAT_MCPPTT_SUBS ORDER BY STIME DESC LIMIT 1"
+        sql = "SELECT * FROM STAT_MCPTT_SUBS ORDER BY STIME DESC LIMIT 1"
         cursor.execute(sql)
 
         data = cursor.fetchall()
 
         for x in data:
-            print(x[0])
-            print(x[1])
-            print(x[2])
+            print(x)
 
         cursor.close()
         conn.close()
     except Exception as msg:
         print('Error Message : %s' %msg)
-
-    # cursor = conn.cursor()
-    #
-    # cursor.execute("select * from alarm_test")
-    #
-    # row = cursor.fetchone()
-    #
-    # while row:
-    #     print("uuid : "+row[0] + ", host : "+row[1]+", msg1 : "+row[2]+", msg2 : "+row[3])
-    #     row = cursor.fetchone()
-    # print()
-    # conn.close()
 
 
 select_all()
